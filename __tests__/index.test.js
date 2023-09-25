90a0647e4741aa6d62b7dcdcb5951cbfdde833da
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-09-25 17:08:17 -0400

    First commit

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,125 @@
-/**
- * Unit tests for the action's entrypoint, src/index.ts
- */
+// index.test.js
+const core = require('@actions/core')
+const fetch = require('isomorphic-fetch')
+const fs = require('fs')
+const { parseStringPromise } = require('xml2js')
+const { fetchRssFeed } = require('../src/index')
 
-const { run } = require('../src/main')
-
-// Mock the action's entrypoint
-jest.mock('../src/main', () => ({
-  run: jest.fn()
+jest.mock('isomorphic-fetch')
+jest.mock('fs', () => ({
+  accessSync: jest.fn(),
+  writeFileSync: jest.fn(),
+  constants: { W_OK: 'some_value' },
+  promises: {
+    access: jest.fn()
+  }
 }))
 
-describe('index', () => {
-  it('calls run when imported', async () => {
-    require('../src/index')
+jest.mock('xml2js')
+jest.mock('@actions/core')
 
-    expect(run).toHaveBeenCalled()
+describe('fetchRssFeed', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
+  it('should set failure if no feed URL is provided', async () => {
+    process.env.INPUT_FEED_URL = ''
+    await fetchRssFeed()
+    expect(core.setFailed).toHaveBeenCalledWith('Feed URL is not provided')
+  })
+
+  it('should set failure if fetch fails', async () => {
+    expect.assertions(1)
+    process.env.INPUT_FEED_URL = 'http://example.com/feed'
+    fetch.mockRejectedValue(new Error('Fetch failed'))
+
+    await fetchRssFeed()
+
+    expect(core.setFailed).toHaveBeenCalledWith('Fetch failed')
+  })
+
+  it('should save the feed to a file if file path is provided', async () => {
+    process.env.INPUT_FEED_URL = 'http://example.com/feed'
+    process.env.INPUT_FILE_PATH = './feed.json'
+    fetch.mockResolvedValue({
+      ok: true,
+      text: jest.fn().mockResolvedValue('<rss></rss>')
+    })
+    parseStringPromise.mockResolvedValue({ rss: {} })
+    fs.accessSync.mockReturnValue(true)
+
+    await fetchRssFeed()
+
+    expect(fs.writeFileSync).toHaveBeenCalledWith(
+      './feed.json',
+      JSON.stringify({ rss: {} }, null, 2)
+    )
+    expect(core.setOutput).toHaveBeenCalledWith(
+      'feed',
+      JSON.stringify({ rss: {} }, null, 2)
+    )
+  })
+
+  it('should not save the feed to a file if no file path is provided', async () => {
+    process.env.INPUT_FEED_URL = 'http://example.com/feed'
+    process.env.INPUT_FILE_PATH = ''
+    fetch.mockResolvedValue({
+      ok: true,
+      text: jest.fn().mockResolvedValue('<rss></rss>')
+    })
+    parseStringPromise.mockResolvedValue({ rss: {} })
+
+    await fetchRssFeed()
+
+    expect(fs.writeFileSync).not.toHaveBeenCalled()
+    expect(core.setOutput).toHaveBeenCalledWith(
+      'feed',
+      JSON.stringify({ rss: {} }, null, 2)
+    )
+  })
+})
+
+// Verify that mocks work correctly
+describe('fetch mock', () => {
+  it('should reject when mockRejectedValue is used', async () => {
+    // Set up the mock to reject
+    fetch.mockRejectedValue(new Error('Fetch failed'))
+
+    // Expect that calling fetch will reject with the specified error
+    await expect(fetch('some_url')).rejects.toThrow('Fetch failed')
+  })
+})
+
+describe('fs mock', () => {
+  it('should call accessSync without errors', () => {
+    fs.accessSync.mockReturnValue(true)
+    expect(() => fs.accessSync('some_path', fs.constants.W_OK)).not.toThrow()
+  })
+
+  it('should call writeFileSync without errors', () => {
+    fs.writeFileSync.mockReturnValue(true)
+    expect(() => fs.writeFileSync('some_path', 'some_data')).not.toThrow()
+  })
+})
+
+describe('xml2js mock', () => {
+  it('should resolve parseStringPromise', async () => {
+    parseStringPromise.mockResolvedValue({ rss: {} })
+    await expect(parseStringPromise('<rss></rss>')).resolves.toEqual({
+      rss: {}
+    })
+  })
+})
+
+describe('@actions/core mock', () => {
+  it('should call setOutput without errors', () => {
+    core.setOutput.mockReturnValue(true)
+    expect(() => core.setOutput('key', 'value')).not.toThrow()
+  })
+
+  it('should call setFailed without errors', () => {
+    core.setFailed.mockReturnValue(true)
+    expect(() => core.setFailed('Some error')).not.toThrow()
   })
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,56 @@
-/**
- * The entrypoint for the action.
- */
-const { run } = require('./main')
+// index.js
+const fetch = require('isomorphic-fetch')
+const fs = require('fs')
+const path = require('path')
+const { parseStringPromise } = require('xml2js')
+const core = require('@actions/core')
 
-run()
+async function fetchRssFeed() {
+  try {
+    const feedUrl = process.env.INPUT_FEED_URL
+    const filePath = process.env.INPUT_FILE_PATH
+
+    if (!feedUrl) {
+      throw new Error('Feed URL is not provided')
+    }
+
+    const response = await fetch(feedUrl)
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch RSS feed (${response.status} ${response.statusText})`
+      )
+    }
+
+    const feedData = await response.text()
+    const parsedData = await parseStringPromise(feedData, {
+      explicitArray: false
+    })
+    const jsonFeedData = JSON.stringify(parsedData, null, 2)
+
+    if (filePath) {
+      const ext = path.extname(filePath)
+      if (ext && ext !== '.json') {
+        throw new Error('File extension must be .json')
+      }
+      const finalFilePath = ext ? filePath : `${filePath}.json`
+      const dir = path.dirname(finalFilePath)
+      fs.accessSync(dir, fs.constants.W_OK)
+      fs.writeFileSync(finalFilePath, jsonFeedData)
+      console.log(`RSS feed saved to ${finalFilePath} successfully!`)
+    } else {
+      console.log('No file path provided; RSS feed not saved to file.')
+    }
+
+    core.setOutput('feed', jsonFeedData)
+  } catch (error) {
+    console.error('Error fetching RSS feed:', error)
+    core.setFailed(error.message)
+  }
+}
+
+module.exports = { fetchRssFeed }
+
+// Run the function if this script is the main module
+if (require.main === module) {
+  fetchRssFeed()
+}
